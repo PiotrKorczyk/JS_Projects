94553748ac549d0d07ddfe1cba5ab3d2f7e6a2a4
PiotrKorczyk <p.k.6@wp.pl>
2017-05-15 21:37:06 +0200

    Drum kit code refactor

--- 01_Drum Kit/script.js
+++ 01_Drum Kit/script.js
@@ -1,63 +1,27 @@
-
-var sounds = ["boom.wav","clap.wav","hihat.wav","kick.wav","openhat.wav",
-"ride.wav","snare.wav","tink.wav","tom.wav"];
-
-var buttons = document.querySelectorAll(".button");
-
-function rem(e){
-	e.classList.add("button-hover");
-	setTimeout((_e =e) =>{_e.classList.remove("button-hover")},150);
+function pressEffect(e){
+	let b = document.querySelector(`#${e}`);
+	b.classList.add("button-hover");
+	setTimeout((_b =b) =>{_b.classList.remove("button-hover")},150);
 }
 
-window.addEventListener("keypress", function makeSound(_key){
-	var track;
-	switch (_key.key) {
-		case "a":
-		track = sounds[1];
-		rem(buttons[0]);
-		break;
-		case "s":
-		track = sounds[2];
-		rem(buttons[1]);
-		break;
-		case "d":
-		track = sounds[3];
-		rem(buttons[2]);
-		break;
-		case "f":
-		track = sounds[4];
-		rem(buttons[3]);
-		break;
-		case "g":
-		track = sounds[0];
-		rem(buttons[4]);
-		break;
-		case "h":
-		track = sounds[5];
-		rem(buttons[5]);
-		break;
-		case "j":
-		track = sounds[6];
-		rem(buttons[6]);
-		break;
-		case "k":
-		track = sounds[8];
-		rem(buttons[7]);
-		break;
-		case "l":
-		track = sounds[7];
-		rem(buttons[8]);
-		break;
+var keys={
+	"a" : ["clap.wav","b0"],
+	"s" : ["hihat.wav","b1"],
+	"d" : ["kick.wav","b2"],
+	"f" : ["openhat.wav","b3"],
+	"g" : ["boom.wav","b4"],
+	"h" : ["ride.wav","b5"],
+	"j" : ["snare.wav","b6"],
+	"k" : ["tom.wav","b7"],
+	"l" : ["tink.wav","b8"],	
+}
 
-		default:
-		track="";
-		break;
-	}
-	if(track!=""){
-		track = "sounds/"+track;
+window.addEventListener("keypress", function makeSound(_key ){
+	if(keys[_key.key] != undefined){
+		let track = `sounds/${keys[_key.key][0]}`;
 		var audio = new Audio(track);
 		audio.play();
+		pressEffect(keys[_key.key][1]);
 	}
-	
 } );
 
